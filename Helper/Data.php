36adebcddd1b7bb263292fbb5483078a66719a6f
boorlyk <viktor@boorlyk.dp.ua>
2022-02-02 11:17:36 +0200

    - fixed issue with wrong composer extra map - removed wrong extending for data helper

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,10 +7,9 @@ namespace Extend\WarrantyGraphQl\Helper;
 use Extend\Warranty\Model\Product\Type as WarrantyType;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Helper\Context;
 
-class Data extends \Extend\Warranty\Helper\Api\Data
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     /**
      * @var ProductRepositoryInterface
@@ -23,26 +22,19 @@ class Data extends \Extend\Warranty\Helper\Api\Data
     protected $searchCriteriaBuilder;
 
     /**
-     * @var ScopeConfigInterface
-     */
-    protected $scopeConfig;
-
-    /**
      * Data constructor.
      * @param ProductRepositoryInterface $productRepository
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      */
     public function __construct(
         Context $context,
-        ScopeConfigInterface $config,
         ProductRepositoryInterface $productRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder
     )
     {
         $this->productRepository = $productRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->scopeConfig = $config;
-        parent::__construct($context, $config);
+        parent::__construct($context);
 
     }
 
@@ -59,60 +51,5 @@ class Data extends \Extend\Warranty\Helper\Api\Data
         $results = $searchResults->getItems();
 
         return reset($results);
-    }
-
-    /**
-     * @param string $field
-     * @return mixed
-     */
-    public function getValue(string $field)
-    {
-        $path = self::BASEPATH . $field;
-        return $this->scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isExtendEnabled()
-    {
-        $path = self::ENABLE_PATH . 'enable';
-        return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isExtendLive()
-    {
-        $path = self::BASEPATH . 'auth_mode';
-        return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isBalancedCart()
-    {
-        $path = self::ENABLE_PATH . 'enableBalance';
-        return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isDisplayOffersEnabled()
-    {
-        $path = self::ENABLE_PATH . 'enableCartOffers';
-        return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return bool
-     */
-    public function isRefundEnabled()
-    {
-        $path = self::ENABLE_PATH . 'enableRefunds';
-        return $this->scopeConfig->isSetFlag($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 }
